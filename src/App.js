43636108f274edeed8ca543c8fcfdaa2fd9c5177
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-17 23:27:50 +0200

    basic structure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,30 @@ import Values from 'values.js'
 
 function App() {
   const [color, setColor] = useState('')
-  return <h2>color generator project</h2>
+  const [error, setError] = useState(false)
+  const [list, setList] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('hellooo');
+  }
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text"
+            placeholder='#f15025'
+            onChange={() => setColor}
+            value={color} />
+          <button className="btn" type='submit'>submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        <h4>List here</h4>
+      </section>
+    </>
+  )
 }
 
 export default App
